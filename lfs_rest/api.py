31d04815510a46783b9207d322daf27a2056f74c
John Goodleaf <jgoodleaf@biotechnikos.net>
2013-03-19 12:25:04 -0700

    Trying to generate a tree view of categories

--- lfs_rest/api.py
+++ lfs_rest/api.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # tastypie imports
 from tastypie import fields
 from tastypie.authorization import Authorization
-from tastypie.authentication import BasicAuthentication
+from tastypie.authentication import Authentication, BasicAuthentication
 from tastypie.resources import ModelResource
 from tastypie.resources import ALL
 from tastypie.serializers import Serializer
@@ -20,7 +20,13 @@ from lfs.catalog.models import Product
 from lfs.customer.models import Customer
 from lfs.order.models import Order
 from lfs.order.models import OrderItem
+from django.forms.models import model_to_dict
 
+from django.conf.urls import url
+from tastypie.http import HttpGone, HttpMultipleChoices
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from tastypie.utils import trailing_slash
+import pprint
 
 class LFSSerializer(Serializer):
     def to_html(self, data, options=None):
@@ -47,18 +53,79 @@ class ProductResource(ModelResource):
 class CategoryResource(ModelResource):
     parent = fields.ForeignKey("lfs_rest.api.CategoryResource", "parent", null=True)
     products = fields.ToManyField("lfs_rest.api.ProductResource", "products", null=True)
-    subcategories = fields.ToManyField("lfs_rest.api.CategoryResource", "subcategories", null=True")
-
+    #categories = fields.ToManyField('self', 'parent', full_list=True, null=True)
+    subcategories = fields.ListField(null=True)
     class Meta:
         queryset = Category.objects.all()
         serializer = LFSSerializer()
         resource_name = 'category'
         authorization = Authorization()
         authentication = Authentication()
-        excludes = ["level", "uid"]
+        excludes = ["level",
+                    "uid",
+                    "product_cols",
+                    "product_rows",
+                    "position",
+                    "static_block",
+                    "template",
+                    "active_formats",
+                    "category_cols",
+                    "meta_description",
+                    "meta_title",
+                    "meta_keywords",
+                    "show_all_products"
+                    ]
         filtering = {
             "name": ALL,
         }
+
+
+    def _strip_category_fields(self, datadictionary):
+        c = CategoryResource()
+        tempdict = {}
+        fields = c.fields.keys()
+
+        for key,val in datadictionary.items():
+            if key in fields:
+                tempdict.update({key:val})
+        return tempdict
+
+    def process_children(self, obj):
+        pp = pprint.PrettyPrinter(indent=2)
+        print "\n\n"
+        print "=" * 60
+        children = obj.get_children()
+        subcategories = []
+        if children:
+            for child in children:
+                #print "=" * 60
+                #print "\tchild: %s\t parent: %s, subcats: %s" % (child, obj, subcategories)
+                tempdict = self._strip_category_fields(model_to_dict(child))
+                p = ProductResource()
+                my_products = []
+                for prod_pk in tempdict['products']:
+                    product = Product.objects.get(pk=prod_pk)
+                    prod_bundle = p.build_bundle(product)
+                    prod_uri = p.get_resource_uri(prod_bundle)
+                    my_products.append(prod_uri)
+                tempdict['products'] = my_products
+                print "\t\t%s" % tempdict
+                subcategories.append(tempdict)
+                #obj.subcategories.append(childresource.full_dehydrate())
+                self.process_children(child)
+        obj.subcategories = subcategories
+        pp.pprint(obj.__dict__)
+        print "+" * 60
+        return obj
+
+    def get_object_list(self, request):
+        top_categories = Category.objects.filter(parent__isnull=True)
+        return top_categories
+
+    def dehydrate(self, bundle):
+        self.process_children(bundle.obj)
+        bundle.data['subcategories'] = bundle.obj.subcategories
+        return bundle
 
 
 class OrderResource(ModelResource):
