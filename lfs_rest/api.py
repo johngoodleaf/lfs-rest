8b41ad49318bd722b38711e70eb03c15af298486
John Goodleaf <jgoodleaf@biotechnikos.net>
2013-03-19 19:59:27 -0700

    Basically working Category tree now, but the code is ugly; there's got to be a better way...

--- lfs_rest/api.py
+++ lfs_rest/api.py
@@ -26,7 +26,7 @@ from django.conf.urls import url
 from tastypie.http import HttpGone, HttpMultipleChoices
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from tastypie.utils import trailing_slash
-import pprint
+from tastypie.bundle import Bundle
 
 class LFSSerializer(Serializer):
     def to_html(self, data, options=None):
@@ -79,53 +79,101 @@ class CategoryResource(ModelResource):
             "name": ALL,
         }
 
-
     def _strip_category_fields(self, datadictionary):
         c = CategoryResource()
         tempdict = {}
         fields = c.fields.keys()
 
-        for key,val in datadictionary.items():
+        for key, val in datadictionary.items():
             if key in fields:
-                tempdict.update({key:val})
+                tempdict.update({key: val})
         return tempdict
 
-    def process_children(self, obj):
-        pp = pprint.PrettyPrinter(indent=2)
-        print "\n\n"
-        print "=" * 60
-        children = obj.get_children()
-        subcategories = []
-        if children:
-            for child in children:
-                #print "=" * 60
-                #print "\tchild: %s\t parent: %s, subcats: %s" % (child, obj, subcategories)
-                tempdict = self._strip_category_fields(model_to_dict(child))
-                p = ProductResource()
-                my_products = []
-                for prod_pk in tempdict['products']:
-                    product = Product.objects.get(pk=prod_pk)
-                    prod_bundle = p.build_bundle(product)
-                    prod_uri = p.get_resource_uri(prod_bundle)
-                    my_products.append(prod_uri)
-                tempdict['products'] = my_products
-                print "\t\t%s" % tempdict
-                subcategories.append(tempdict)
-                #obj.subcategories.append(childresource.full_dehydrate())
-                self.process_children(child)
-        obj.subcategories = subcategories
-        pp.pprint(obj.__dict__)
-        print "+" * 60
-        return obj
+    def _get_product_uris(self, product_list):
+        p = ProductResource()
+        my_products = []
+        for prod_pk in product_list:
+            product = Product.objects.get(pk=prod_pk)
+            prod_bundle = p.build_bundle(product)
+            prod_uri = p.get_resource_uri(prod_bundle)
+            my_products.append(prod_uri)
+        return my_products
 
-    def get_object_list(self, request):
-        top_categories = Category.objects.filter(parent__isnull=True)
-        return top_categories
+    def process_children(self, obj):
+        subcategories = obj.get_children()
+        if not subcategories:
+            obj.subcategories = []
+            #return obj
+        else:
+            obj.subcategories = []
+            for category in subcategories:
+                tempdict = self._strip_category_fields(model_to_dict(category))
+                tempdict['products'] = self._get_product_uris(tempdict['products'])
+                obj.subcategories.append(tempdict)
+                self.process_children(category)
+            #return obj
+
+    # def get_object_list(self, request):
+    #     top_categories = Category.objects.filter(parent__isnull=True)
+    #     return top_categories
 
     def dehydrate(self, bundle):
         self.process_children(bundle.obj)
         bundle.data['subcategories'] = bundle.obj.subcategories
         return bundle
+
+    def _get_id(self, uri_string):
+        try:
+            return int(uri_string.split('/')[3])
+        except AttributeError:
+            return None
+
+    def _append_or_replace(self, blist, val):
+        for b in blist:
+            if isinstance(b, dict):
+                blist.remove(b)
+        if val in blist:
+            blist.remove(val)
+        blist.append(val)
+
+    def search_bundle(self, obj_list):
+        for o in obj_list:
+            if isinstance(o, Bundle):
+                subcats = o.data['subcategories']
+                o.data['subcategories'] = self.strip_dicts(subcats)
+
+    def strip_dicts(self, obj_list):
+        return [thing for thing in obj_list if isinstance(thing, Bundle)]
+
+    def alter_list_data_to_serialize(self, request, data):
+        bundles = data['objects']
+        bundle_dict = {}
+        final_bundle = {}
+
+        for bundle in bundles:
+            bundle_dict[bundle.data['id']] = bundle
+
+        with_parents = [v for v in bundle_dict.values() if v.data['parent']]
+        without_parents = [v for v in bundle_dict.values() if not v in with_parents]
+
+        for v in without_parents:
+            final_bundle[v.data['id']] = v
+
+        for v in with_parents:
+            id_val = self._get_id(v.data['parent'])
+            try:
+                parent = final_bundle[id_val]
+            except KeyError:
+                parent = bundle_dict[id_val]
+            self._append_or_replace(parent.data['subcategories'], v)
+
+        # for k, v in final_bundle.data.items():
+        #     if type(v) is "dict":
+        #         del(final_bundle[k])
+        #     elif type(v) is "Bundle" and v.data['subcategories']:
+        #         del()
+        data['objects'] = final_bundle.values()
+        return data
 
 
 class OrderResource(ModelResource):
