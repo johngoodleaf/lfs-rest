7020825ee898b79316809e780558e8b78c1ff60e
John Goodleaf <john@goodleaf.net>
2013-03-26 12:42:31 -0700

    Fixing httpresponse objects so it won't error out

--- lfs_rest/views.py
+++ lfs_rest/views.py
@@ -1,5 +1,6 @@
 #views for order status
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 try:
     import json
 except ImportError:
@@ -9,9 +10,12 @@ from django.http import HttpResponse
 from lfs.catalog.models import Product
 import requests
 
-
+@csrf_exempt
 def submitted(request):
     if request.POST:
         print request.POST
+        return HttpResponse(
+            json.dumps(request.POST['products'],
+                content_type="application/json")
     else:
         return HttpResponse()
