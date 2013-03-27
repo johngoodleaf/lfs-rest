47bf5458fb8602eb74c1f6edc98d578c0b949351
John Goodleaf <john@goodleaf.net>
2013-03-27 12:40:27 -0700

    Added first pass at pusher support

--- lfs_rest/views.py
+++ lfs_rest/views.py
@@ -9,13 +9,20 @@ from django.http import HttpResponse
 
 from lfs.catalog.models import Product
 import requests
+import pusher
 
 @csrf_exempt
 def submitted(request):
     if request.method == "POST":
         products = json.loads(request.raw_post_data)
         print request.raw_post_data
+
         print products
+        p = pusher.Pusher(app_id='40239',
+            key='1ebb3cc2881a1562cc37',
+            secret='7296ddd9aede74695af1')
+
+        p['order_channel'].trigger('my_event', {'products': products})
         return HttpResponse(json.dumps(products),
             content_type="application/json")
     else:
