0a7602bf4e5942d2481b0b06fc9330272be4bd1b
John Goodleaf <john@goodleaf.net>
2013-03-27 15:45:48 -0700

    Starting to build foundation for carts and price summaries

--- lfs_rest/views.py
+++ lfs_rest/views.py
@@ -15,9 +15,15 @@ import pusher
 def submitted(request):
     if request.method == "POST":
         products = json.loads(request.raw_post_data)
-        print request.raw_post_data
+        product_list = products['products']
 
-        print products
+        print request.raw_post_data
+        print product_list
+
+        for p in product_list:
+            product = Product.objects.get(pk=p['id'])
+            print product
+
         p = pusher.Pusher(app_id='40239',
             key='1ebb3cc2881a1562cc37',
             secret='7296ddd9aede74695af1')
