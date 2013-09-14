92b5595cbafee9dc64a8b5375a90597763239cfd
John Goodleaf <jgoodleaf@biotechnikos.net>
2013-09-13 17:20:24 -0700

    Fixing header bug

--- lfs_rest/views.py
+++ lfs_rest/views.py
@@ -27,6 +27,8 @@ def check_auth(request):
     print headers
     r = requests.get('http://localhost:8001/customer/api-token-auth/',
         headers=headers)
+    print r.json()
+    print r.text
     return r
 
 
